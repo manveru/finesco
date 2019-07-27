14859c3bf2d3b3234c79f5c0123f84731f303372
Michael Fellinger <michael.fellinger@xing.com>
2019-07-28 01:29:40 +0200

    use nixpkgs hakyll

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,11 +1,14 @@
 { pkgs ? import ./nixpkgs.nix, default ? import ./. }:
+let
+  ghc = pkgs.haskell.packages.ghc865.ghcWithPackages (ps: with ps; [ hakyll ]);
+in
 pkgs.dockerTools.buildLayeredImage {
-  name = "finesco";
+  name = "hakyll";
   maxLayers = 100;
   contents = [
     pkgs.ncdu
     pkgs.vim
     pkgs.bashInteractive
-    default.finesco.components.exes.site
+    ghc
   ];
 }
--- shell.nix
+++ shell.nix
@@ -1,24 +1,10 @@
 let
   pkgs = import ./nix/nixpkgs.nix;
-  hsPkgs = import ./nix/default.nix;
-  haskell-nix = import ./nix/haskell.nix { inherit pkgs; };
-
-  haskellDevPkgs = with pkgs.haskell.packages.ghc865; [
-    haskell-nix.nix-tools
-    cabal-install
-    ghcid
-    stylish-haskell
-    brittany
-  ];
-
-  devPkgs = with pkgs; [ ruby_2_6 watchexec entr sass cacert ];
-in hsPkgs.shellFor {
-  packages = ps: with ps; [ finesco ];
-  withHoogle = true;
-
-  buildInputs = devPkgs ++ haskellDevPkgs;
-
+  ghc = pkgs.haskell.packages.ghc865.ghcWithPackages (ps: with ps; [ hakyll ]);
+in pkgs.mkShell {
+  buildInputs = [ ghc ];
   shellHook = ''
+    eval $(grep ^export ${ghc}/bin/ghc)"
     unset preHook
   '';
 }
