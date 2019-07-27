da7219311b690e5c1aeff11981a69a222a3ec032
Michael Fellinger <michael.fellinger@xing.com>
2019-07-28 00:11:55 +0200

    prepare for netlify

--- site.hs
+++ site.hs
@@ -18,6 +18,10 @@ main = hakyll $ do
     route idRoute
     compile copyFileCompiler
 
+  match "admin/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
   match "js/*" $ do
     route idRoute
     compile copyFileCompiler
