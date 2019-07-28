40184500a9877fbebfb2a88cb364111ffe7c7322
Michael Fellinger <michael.fellinger@xing.com>
2019-07-28 02:19:59 +0200

    fix locale, add sass

--- shell.nix
+++ shell.nix
@@ -2,7 +2,10 @@ let
   pkgs = import ./nix/nixpkgs.nix;
   ghc = pkgs.haskell.packages.ghc865.ghcWithPackages (ps: with ps; [ hakyll ]);
 in pkgs.mkShell {
-  buildInputs = [ ghc ];
+  buildInputs = [ ghc pkgs.locale pkgs.sass ];
+  LOCALE_ARCHIVE = "${pkgs.buildPackages.glibcLocales}/lib/locale/locale-archive";
+
+  LC_ALL = "en_US.UTF-8";
   shellHook = ''
     eval $(grep ^export ${ghc}/bin/ghc)"
     unset preHook
